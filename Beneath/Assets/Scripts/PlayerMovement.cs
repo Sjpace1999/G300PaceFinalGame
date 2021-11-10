5b91fe7b99067a66d5f506c1127f17657d29cabd
Sjpace1999 <sjpace1999@gmail.com>
2021-11-10 15:10:53 -0500

    2nd video complete

--- Beneath/Assets/Scripts/PlayerMovement.cs
+++ Beneath/Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public LayerMask groundLayer;
     public float runningSpeed;
     public float jumpSpeed;
     private Rigidbody2D body;
+    private Animator anim;
+    private BoxCollider2D boxCollider;
     // Start is called before the first frame update
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     void Start()
@@ -22,11 +27,40 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * runningSpeed,body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * runningSpeed,body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
+        if (horizontalInput> 0)
         {
-            body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+            transform.localScale = Vector3.one;
         }
+        else if(horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
+        {
+            Jump();
+        }
+
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", isGrounded());
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+        anim.SetTrigger("jump");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size,0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
     }
 }
