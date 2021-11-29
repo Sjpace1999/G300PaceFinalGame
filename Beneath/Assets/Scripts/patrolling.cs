cef95e6227136bd3d01c0fec3c1632222baaef45
Sjpace1999 <sjpace1999@gmail.com>
2021-11-29 13:47:20 -0500

    Bird Waypointing

--- Beneath/Assets/Scripts/patrolling.cs
+++ Beneath/Assets/Scripts/patrolling.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class patrolling : MonoBehaviour
 {
     public Transform[] positions;
+    private Rigidbody2D body;
+    int x=0;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, positions[x].position, Time.deltaTime);
+        if (transform.position.x == positions[x].position.x && transform.position.y == positions[x].position.y)
+        {
+            x = (x + 1) % positions.Length;
+        }
+        Debug.Log(x);
     }
 }
